e6a45efa6911b4a8e82a916924ac46d8fad44bbc
Daniel Galan Navio <d.galan@necsia.es>
2020-03-13 10:56:49 +0100

    added functionality to delete notes, trying to animate enter and leave of notes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./bootstrap.min.css";
 import AddNote from "./components/AddNote";
 import Notes from "./components/Notes";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [state, setState] = useState({
@@ -12,7 +12,7 @@ function App() {
       {
         id: uuidv4(),
         data: "The Earth is not flat"
-      }, 
+      },
       {
         id: uuidv4(),
         data: "It's not round either"
@@ -28,16 +28,22 @@ function App() {
     const newNote = {
       id: uuidv4(),
       data
-    }
+    };
     // newNote.id = uuidv4(); //state.notes[state.notes.length].id + 1;
-    setState({notes: [...state.notes, newNote]});
-  }
+    setState({ notes: [...state.notes, newNote] });
+  };
+
+  const delNote = id => {
+    setState({
+      notes: [...state.notes.filter(note => note.id != id)]
+    });
+  };
 
   return (
     <div className="container" style={{ marginTop: "10vh" }}>
       <h1 style={titleStyle}>My Notes</h1>
-      <div className="d-flex" style={{margin: '15px'}}>
-        <Notes notes={state.notes} />
+      <div className="d-flex" style={{ margin: "15px" }}>
+        <Notes notes={state.notes} delNote={delNote} />
       </div>
       <AddNote addNote={addNote} />
     </div>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Note from "./Note";
+import { CSSTransition } from "react-transition-group";
+import "./styles/Note.css";
 
 export default function Notes(props) {
   return props.notes.map(note => {
-    return <Note note={note} />;
+    return (
+      <CSSTransition className="note" timeout={200}>
+        <Note note={note} delNote={props.delNote} />
+      </CSSTransition>
+    );
   });
 }
